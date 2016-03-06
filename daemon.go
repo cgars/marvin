7e9e5967f98968bc2d283bf49ce48afed6671894
Christian Kellner <christian@kellner.me>
2016-03-06 22:47:11 +0100

    Can connect, fetches meals on "mensa", not more

---
+++ daemon.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "crypto/tls"
+    "fmt"
+    "strings"
+
+    irc "github.com/fluffle/goirc/client"
+    "github.com/gnode/marvin/mensa"
+)
+
+type Bot struct {    
+    conn *irc.Conn
+    
+    quit chan bool
+}
+
+
+func (b *Bot) onConncted(conn *irc.Conn, line *irc.Line) {
+    conn.Join("#gnode");   
+}
+
+func (b *Bot) onPrivMessage(conn *irc.Conn, line *irc.Line) {
+    text := line.Text()
+    target := line.Target()
+    
+    fmt.Printf("[D] {pm}: [%s] %s\n", target, text)
+    
+    if strings.HasPrefix(text, "mensa") {
+        mc := &mensa.Client{Address: "http://openmensa.org/api/v2"}
+        meals, _ := mc.MealsForToday("134")
+        // ignored error for now
+        
+        if len(meals) == 0 {
+            conn.Privmsgf(target, "No milk today, my love has gone away...")
+            return
+        }
+        
+        for _, meal := range meals {
+            if !strings.HasPrefix(meal.Category, "Tagesgericht") {
+                continue
+            }
+            notes := strings.Join(meal.Notes, ", ")
+            conn.Privmsgf(target, "%s [%s]", meal.Name, notes)
+        }
+    }
+}
+
+func NewBot() *Bot {
+    cfg := irc.NewConfig("gnode", "marvin", "Metal Man")
+    cfg.SSL = true
+    cfg.SSLConfig = &tls.Config{ServerName: "irc.freenode.net"}
+    cfg.Server = "irc.freenode.net:7000"
+    cfg.NewNick = func(n string) string { return n + "^" }
+    cfg.Version = "0.1"
+    cfg.QuitMessage = "Oh god, I am so depressed!"
+
+    c := irc.Client(cfg) 
+    b := &Bot{conn: c}
+    b.quit = make(chan bool)
+    
+    c.HandleFunc(irc.CONNECTED, b.onConncted)
+    c.HandleFunc(irc.DISCONNECTED,
+        func(conn *irc.Conn, line *irc.Line) { b.quit <- true })
+        
+    c.HandleFunc(irc.PRIVMSG, b.onPrivMessage)
+        
+    return b
+}
+
+
+func main() {
+ 
+    b := NewBot()
+  
+    if err := b.conn.Connect(); err != nil {
+        fmt.Printf("Connection error: %s\n", err.Error())
+    }
+    
+    <-b.quit
+}
---
+++ mensa/mensa.go
@@ -0,0 +1,66 @@
+package mensa
+
+import (
+	"encoding/json"
+	"fmt"
+    "time"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+    Address string
+}
+
+
+type Meal struct {
+    Id int
+    Name string
+    Category string
+    Notes []string
+}
+
+func (mensa *Client) Meals(canteen string, day string) ([]Meal, error) {
+    client := &http.Client{}
+
+    meals := make([]Meal, 0)
+
+	url := fmt.Sprintf("%s/canteens/%s/days/%s/meals",
+                        mensa.Address, canteen, day)
+
+	res, err := client.Get(url)
+	if err != nil {
+		return meals, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return meals, err
+	}
+
+	if err = json.Unmarshal(body, &meals); err != nil {
+		return meals, err
+	}
+
+	return meals, nil
+}
+
+func (mensa *Client) MealsForToday(canteen string) ([]Meal, error) {
+    now := time.Now()
+    
+    day := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+    fmt.Printf("%s\n", day)
+    return mensa.Meals(canteen, day)
+}
+
+func main() {
+    client := &Client{Address: "http://openmensa.org/api/v2"}
+    res, err := client.Meals("134", "2016-03-04")
+    if err != nil {
+        println(err)
+    }
+    
+    fmt.Printf("%v\n", res)
+}
